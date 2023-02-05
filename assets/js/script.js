e117656e1bf12197420679b1b0d23050b14c2a98
Farid <farid.adnaoui.developer@outlook.fr>
2023-02-05 20:55:35 +0100

    all steps html done and logic javascript is ok can update steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,46 @@
-var global = {
+let formStepsNum = 0;
+const formSteps = document.querySelectorAll(".wizard-step")
+const progressSteps = document.querySelectorAll(".wizard-navigation__list__item")
+
+var wizard = {
     init: function () {
         console.log("😎")
+    },
+    buttons: function () {
+        const button = [...document.getElementsByClassName("button-action")]
+
+        console.log("item")
+        button.map(item => {
+            item.addEventListener("click", function () {
+
+                if (this.classList.contains("next")) {
+                    // if (formStepsNum > (progressSteps.length - 2)) return false
+
+                    formStepsNum++
+                } else {
+                    formStepsNum--
+                }
+
+                wizard.updateFormSteps()
+                wizard.updateProgressbar()
+            })
+        })
+    },
+    updateFormSteps: function () {
+        formSteps.forEach((formStep) => {
+            formStep.classList.contains("active") &&
+                formStep.classList.remove("active")
+        });
+
+        formSteps[formStepsNum].classList.add("active")
+    },
+    updateProgressbar: function () {
+        progressSteps.forEach((progressStep, idx) => {
+            if (idx < formStepsNum + 1) {
+                progressStep.classList.add("active")
+            } else {
+                progressStep.classList.remove("active")
+            }
+        });
     },
 }
